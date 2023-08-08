0d8717932115c2e6dbb23bc50f0c6d2cb1e35d15
Pablo Mendez <mendez4a@gmail.com>
2023-08-08 17:07:25 +0200

    remove uneeded libraries

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
 import React, { useState, useEffect } from "react";
-import ReconnectingWebSocket from "reconnecting-websocket";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [ws, setWS] = useState(null);
 
   useEffect(() => {
-    const rws = new ReconnectingWebSocket("ws://localhost:6000");
+    const websocket = new WebSocket("ws://localhost:5000");
 
-    rws.addEventListener("open", () => {
+    websocket.onopen = () => {
       console.log("Connected to the server");
-      rws.send("getData"); // Request the mock data once connected.
-    });
+    };
 
-    rws.addEventListener("message", (event) => {
+    websocket.onmessage = (event) => {
       console.log("Message from server: ", event.data);
       const data = JSON.parse(event.data);
       setMessage(data.message);
-    });
+    };
+
+    websocket.onerror = (error) => {
+      console.error("WebSocket Error:", error);
+    };
+
+    websocket.onclose = (event) => {
+      if (event.wasClean) {
+        console.log(
+          `Closed cleanly, code=${event.code}, reason=${event.reason}`
+        );
+      } else {
+        console.error("Connection died");
+      }
+    };
+
+    setWS(websocket);
 
     return () => {
-      rws.close();
+      websocket.close();
     };
   }, []);
+
+  const handleGetData = () => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send("getData"); // Request the mock data when button is clicked.
+    }
+  };
 
   return (
     <div className="App">
       <h1>React WebSockets</h1>
+      <button onClick={handleGetData}>Get Data from Backend</button>
       <p>{message}</p>
     </div>
   );
